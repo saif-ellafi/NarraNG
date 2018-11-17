eb42d3345fac63756f744049c7ad3308237d79e0
Saif Addin <saif@johnsnowlabs.com>
2018-11-17 18:47:50 -0300

    Added -generate-n-of-each-

--- narrator/narrang.py
+++ narrator/narrang.py
@@ -17,6 +17,7 @@ class Narrator:
     LAST_NODE = 1
     LEAF = 2
 
+    # Initializes project, path recognition and user input
     def __init__(self, project, name=None):
         self.name = name if name else project
         self.project = project
@@ -35,12 +36,14 @@ class Narrator:
                 """)
         logging.debug("DEBUG MODE ON")
 
+    # Represents a narrative selection
     class Selection:
         def __init__(self, name, path, state):
             self.name = name
             self.path = path
             self.state = state
 
+    # Represents project walk through and selection generation
     class Wizard:
         def __init__(self):
             self.selections = []
@@ -56,6 +59,7 @@ class Narrator:
                 logging.debug("Generated entry with name: " + str(name) + " at path: " + str(path))
                 self.selections.append(Narrator.Selection(name, path, state=1 if is_leaf else 0))
 
+    # Represents final file node walk through
     class LeafWizard(Wizard):
         def __init__(self):
             super(Narrator.LeafWizard, self).__init__()
@@ -66,32 +70,16 @@ class Narrator:
                     name = (callback_name + ' ' + line if callback_name else line).strip()
                     self.selections.append(Narrator.Selection(name, path, state=2))
 
-    @staticmethod
-    def input_callback(max_index):
-        while True:
-            choice = input().strip()
-            if choice == '0':
-                logging.debug("exit option selected")
-                return 0
-            elif not choice:
-                logging.debug("single random shot. Setting randomized choices to 1")
-                return random.randint(1, max_index)
-            else:
-                logging.debug("standard input chosen")
-                try:
-                    if 0 < int(choice) <= max_index:
-                        return int(choice)
-                except ValueError:
-                    if choice:
-                        return choice
-
+    # Clears current selections
     def clear(self):
         logging.debug("Clearing build")
         self.build = []
 
+    # Deletes a specific selection
     def delete(self, index):
         del self.build[index-1]
 
+    # Given a specific selection, handles next step
     def resolve(self, selection, auto=False):
         if selection.state == Narrator.ROOT:
             logging.debug("Go back to wizard, since selection is not final")
@@ -106,6 +94,33 @@ class Narrator:
             logging.debug("result is set from standard input")
             self.build.append(selection)
 
+    # Handles user's input and validations
+    @staticmethod
+    def input_callback(max_index):
+        while True:
+            choice = input().strip()
+            if choice == '0':
+                logging.debug("exit option selected")
+                return 0
+            elif choice.startswith('#'):
+                try:
+                    int(choice[1:])
+                    return choice
+                except ValueError:
+                    pass
+            elif not choice:
+                logging.debug("single random shot. Setting randomized choices to 1")
+                return random.randint(1, max_index)
+            else:
+                logging.debug("standard input chosen")
+                try:
+                    if 0 < int(choice) <= max_index:
+                        return int(choice)
+                except ValueError:
+                    if choice:
+                        return choice
+
+    # Creates a wizard and starts user interaction for selections
     def gen(self, wizard=Wizard, root_path=None, callback_name=None, auto=False):
         wiz = wizard()
         path = root_path if root_path else self.project_path
@@ -122,17 +137,25 @@ class Narrator:
             user_choice = Narrator.input_callback(len(wiz.selections))
         if user_choice == 0:
             return
+        elif str(user_choice).startswith('#'):
+            n = int(user_choice[1:])
+            for i in range(0, len(wiz.selections)):
+                for ii in range(0, n):
+                    self.resolve(wiz.selections[i], auto=True)
         elif type(user_choice) == str:
             self.resolve(self.Selection(user_choice, '', self.LEAF), auto=auto)
         else:
             self.resolve(wiz.selections[user_choice-1], auto=auto)
 
+    # Creates n wizards for multiple automated selections
     def mgen(self, n, wizard=Wizard, root_path=None, callback_name=None):
         i = 0
         while i < n:
             self.gen(wizard, root_path, callback_name, auto=True)
             i += 1
+        self.print()
 
+    # How is Narrative represented in console
     def __repr__(self):
         return '''--------------------\nProject {} has:\n--------------------\n{}'''. \
             format(self.name, '\n'.join([str(index+1) + '. ' + selection.name for index, selection in enumerate(self.build)]))
